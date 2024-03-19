c701e29b2a612e24007e4f1e28bd384c62f73ec0
Daniela Ramos <daniramos5566@hotmail.com>
2024-03-19 11:01:35 -0600

    added pink color

--- act3_juegoPintado.py
+++ act3_juegoPintado.py
@@ -38,7 +38,15 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
+
+    end_fill()
 
 
 def rectangle(start, end):
@@ -79,7 +87,9 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
-onkey(lambda: store('shape', line), 'l')
+#added red line filled with pink color when pressed the letter P
+onkey(lambda: color('red','pink'), 'P')
+onkey(lambda: store('shape', line), 'l')	
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
