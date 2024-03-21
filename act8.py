3f0fe3e2589000817765bf1782006001523c2fd5
gaelgarcia003 <105666768+AngelGaelGarciaRangel@users.noreply.github.com>
2024-03-21 11:04:29 -0600

    added funcionality with words

--- act8.py
+++ act8.py
@@ -10,7 +10,13 @@ Exercises:
 """
 
 #Daniela Ramos Garcia A01174259
-
+#Ángel Gael García Rangel A00833115
+""" 
+Instrucciones:
+Antes de comenzar el juego, la línea de comandos te preguntará si quieres usar letras o números. 
+Una vez que coloques qué tipo de dato quieres usar, el juego comenzará.
+Encuentra los 32 pares pafra desbloquear la imagen.
+"""
 from random import *
 from turtle import *
 
@@ -21,7 +27,7 @@ tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
 tab_count = 0
-
+onLetters = False
 def square(x, y):
     """Draw white square with black outline at (x, y)."""
     up()
@@ -79,11 +85,14 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        if tiles[mark] < 10:
-            center = 17  # Center single-digit numbers
+        if onLetters:
+            goto(x, y + 7)
         else:
-            center = 10  # Center two-digit numbers
-        goto(x + center, y + 7)
+            if tiles[mark] < 10:
+                center = 17  # Center single-digit numbers
+            else:
+                center = 10  # Center two-digit numbers
+            goto(x + center, y + 7)
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
@@ -107,7 +116,12 @@ def draw():
         
     ontimer(draw, 100)
 
-
+option = input("If you want to use letters write L, write any other letter to use numbers: ")
+if option == "L":
+    tiles = ['cat', 'dog', 'hat', 'car', 'run', 'fun', 'sun', 'red', 'big', 'pen', 
+                'cup', 'top', 'cup', 'box', 'boy', 'toy', 'sky', 'jam', 'mix', 'hot', 
+                'man', 'pet', 'mat', 'mad', 'zip', 'dip', 'map', 'bat', 'leg', 'pig', 'rug', 'lup'] * 2
+    onLetters = True
 shuffle(tiles)
 setup(480, 480, 370, 0)
 addshape(car)
